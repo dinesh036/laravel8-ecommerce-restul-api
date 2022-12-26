61f99f73ac58640acf038dcdee916fa1a3fec531
Dinesh Sharma <dinesh.sharma036@gmail.com>
2022-12-26 10:25:45 +0530

    product resources created

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function review(){
+        return $this->hasMany(Review::class);
+    }
 }
