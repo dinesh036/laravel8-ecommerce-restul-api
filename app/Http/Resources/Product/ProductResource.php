ad841daf6b227a93efb2486b073c8575300143e0
Dinesh Sharma <dinesh.sharma036@gmail.com>
2022-12-26 11:12:32 +0530

    modify product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,13 @@ class ProductResource extends JsonResource
             'name'          =>  $this->name,
             'description'   =>  $this->detail,
             'price'         =>  $this->price,
-            'stock'         =>  $this->stock,
-            'discount'      =>  $this->discount
+            'stock'         =>  $this->stock==0?'Out of stock':$this->stock,
+            'totalPrice'    =>  round((1-($this->discount/100))*$this->price,2),
+            'discount'      =>  $this->discount,
+            'rating'        =>  ($this->review()->count()>0)?($this->review()->sum('star')/$this->review()->count('star')):'No Rating Yat',
+            'href' => [
+                'link'=> route('reviews.index',$this->id)
+            ]
         ];
     }
 }
